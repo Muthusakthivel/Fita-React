bd2850c977680164ac21a4afd439c8153f02367b
muthukumar <muthukumar.s@piquota.com>
2022-11-17 10:14:56 +0530

    Post

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -19,6 +19,7 @@ class Home extends Component{
                 userList : serverResult.data
             })
             console.log(serverResult.data);
+            console.log(response.data);
         })
         .catch((error)=> {
             console.log(error);
--- src/Pages/inbox.js
+++ src/Pages/inbox.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class InboxPage extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            name: '',
+            job : ''
+        }
+    }
+    onHandleInput = (event) => {
+        this.setState({
+         [event.target.name]: event.target.value
+        })
+    }
+    onSubmit = () => {
+        const url = "https://reqres.in/api/users";
+
+        axios.post(url, this.state)
+        .then((response)=>{
+              const serverData = response.data;
+            console.log(serverData);
+            alert("Successfully User profile has been Created!");
+        })
+        .catch((error) => {
+            console.log(error);
+            alert("Something went wrong");
+        })
+    }
     render() {
         return(
             <div>
                 <h1>This is a Inbox Page</h1>
+                <div>
+                    <label>Enter Your Name :</label>
+                    <input type="text" placeholder='Please enter your name'
+                    onChange={this.onHandleInput} name="name"/>
+                </div>
+                <div>
+                <label>Enter Your Job Role :</label>
+                    <input type="text" placeholder='Please enter your Job Role'
+                    onChange={this.onHandleInput} name="job" />
+                </div>  
+                <button onClick={() => this.onSubmit()}>Submit User</button>
             </div>
         )
     }
