dad2cc3a0de83d785fd0bafea4aa7a52bb4f9f62
muthukumar <muthukumar.s@piquota.com>
2022-11-21 09:41:47 +0530

    Put

--- src/Pages/draft.js
+++ src/Pages/draft.js
@@ -1,12 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component} from "react";
+import axios from "axios";
 
-class DraftPage extends Component {
-    render() {
-        return(
-            <div>
-                <h1>This is a Draft Page</h1>
-            </div>
-        )
-    }
+class DraftPage extends Component{
+
+  onDeleteUser(){
+    const url = "https://reqres.in/api/users/2";
+
+    axios.delete(url)
+      .then((response) => {
+        const serverData = response.data;
+        console.log(serverData);
+        alert("Successfully Deleted the User Profile");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Something went wrong")
+      })
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>THis is a Draft Page</h1>
+        <button onClick={() => this.onDeleteUser()}>Delete User Account</button>
+      </div>
+    )
+  }
 }
+
 export default DraftPage;
--- src/Pages/sent.js
+++ src/Pages/sent.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SentPage extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            name : '',
+            job : ''
+        }
+    }
+
+    onHandleInput = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    onUpdate () {
+       const url = "https://reqres.in/api/users/2";
+
+       axios.put(url, this.state)
+       .then((response) => {
+        const serverData = response.data;
+        console.log(serverData);
+        alert("Updated User Profile Successfully");
+       })
+       .catch((error) => {
+        console.log(error);
+       })
+
+    }
     render() {
         return(
             <div>
                 <h1>This is a Sent Page</h1>
+                <div>
+                <label>Update Your Name</label>
+                <input type="text" placeholder='Please Update Your Name'  onChange = {this.onHandleInput} name='name' />
+               
+                </div>
+                <div>
+                <label>Update Your Job Role</label>
+                <input type="text" placeholder='Please Update Your Job Role' onChange = {this.onHandleInput} name='job' />
+                
+                </div>
+                <button onClick={()=>this.onUpdate()}>Update profile</button>
             </div>
         )
     }
