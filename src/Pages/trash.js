b8b77f3393f3b68bd40e28178e9c2ffa2c099f2c
muthukumar <muthukumar.s@piquota.com>
2022-11-28 09:43:45 +0530

    Props

--- src/Pages/trash.js
+++ src/Pages/trash.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import ChildComponent from '../Components/child';
 
 class TrashPage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            message : ''
+        }
+    }
+    onReceiveValue = (value) => {
+        console.log(value);
+        this.setState({
+            message : value
+        })
+    }
     render() {
+
+        const data = {
+            name: 'Mr.ABC',
+            age: 23
+        }
         return(
             <div>
-                <h1>This is a Trash Page</h1>
+                <h2>Received Username - {this.state.message}</h2>
+                <ChildComponent information={data} callback={this.onReceiveValue}></ChildComponent>
             </div>
         )
     }
