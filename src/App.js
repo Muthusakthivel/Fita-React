9317b1d1b4d441ee0c90c7af3ad8f7603e5ecfee
muthukumar <muthukumar.s@piquota.com>
2022-12-20 10:27:39 +0530

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import SimpleFormSubmition from './Components/simple-form';
 import './Styles/ui-style.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+
+import HomePage from './Pages/home';
+import ContactPage from '../src/Pages/contact';
+import AboutPage from '../src/Pages/about';
 
 function App(){
   return(
-    <div>
-      <h1>Welcome to React</h1>
-      <SimpleFormSubmition></SimpleFormSubmition>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage></HomePage>}></Route>
+        <Route path='contact' element={<ContactPage></ContactPage>}></Route>
+        <Route path='about' element={<AboutPage></AboutPage>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 export default App;
