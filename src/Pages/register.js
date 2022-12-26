0f9ee6010313c81d5357f9dc24214e7d0a5ca64f
muthukumar <muthukumar.s@piquota.com>
2022-12-26 10:23:20 +0530

    Register Page

--- src/Pages/register.js
+++ src/Pages/register.js
@@ -13,49 +13,49 @@ const RegisterPage = () => {
         qualification: ''
     });
     const submitRegister = () => {
-
+        console.log(register);
     }
     const handleInput = (event) => {
-        console.log(event.target.value);
+        setRegister({...register, [event.target.name]: event.target.value});
     }
     return(
         <div>
             <h1>Register Page</h1>
             <div className='space'>
                 <label className='register-label'>Enter Your Name :</label>
-                <input type="text" placeholder="Enter your name" className='register-input' onChange={handleInput}/>
+                <input type="text" placeholder="Enter your name" className='register-input' onChange={handleInput} name='userName'/>
             </div>
             <div className='space'>
                 <label className='register-label'>Enter Your Email ID :</label>
-                <input type="text" placeholder="Enter Your Email ID" className='register-input' onChange={handleInput}/>
+                <input type="text" placeholder="Enter Your Email ID" className='register-input' onChange={handleInput} name='emailId'/>
             </div>
             <div className='space'>
                 <label className='register-label'>Enter Your Password :</label>
-                <input type="password" placeholder="Enter Your Password" className='register-input' onChange={handleInput}/>
+                <input type="password" placeholder="Enter Your Password" className='register-input' onChange={handleInput} name='password'/>
             </div>
             <div className='space'>
                 <label className='register-label'>Select Your DOB : </label>
-                <input type="date"  className='register-input' onChange={handleInput}/>
+                <input type="date"  className='register-input' onChange={handleInput} name='dateOfBirth'/>
             </div>
             <div className='space'>
                 <label className='register-label'>Select Your Gender : </label>
-                <input type="radio" name='gender' className='register-selection' onChange={handleInput} value="Male"/>Male
+                <input type="radio" name='gender' className='register-selection' onChange={handleInput} value="Male" />Male
                 <input type="radio" name='gender' className='register-selection' onChange={handleInput} value="Female"/>Female
                 <input type="radio" name='gender' className='register-selection' onChange={handleInput} value="others"/>Others
             </div>
             <div className='space'>
                 <label className='register-label'>Select Your Hobbies : </label>
-                <input type="checkbox" onChange={handleInput} value="cricket"/>Cricket
-                <input type="checkbox" onChange={handleInput} value="Badminton"/>Badminton
-                <input type="checkbox" onChange={handleInput} value="Football"/>Football
+                <input type="checkbox" onChange={handleInput} value="cricket" name='hobbies'/>Cricket
+                <input type="checkbox" onChange={handleInput} value="Badminton" name='hobbies'/>Badminton
+                <input type="checkbox" onChange={handleInput} value="Football" name='hobbies'/>Football
             </div>
             <div className='space'>
                 <label className='register-label'>Enter Your Address</label>
-                <textarea name='address' placeholder='Enter your address'/>
+                <textarea name='address' placeholder='Enter your address' className='register-input' onChange={handleInput}></textarea>
             </div>
             <div className='space'>
                 <label className='register-label'>Select your Highest Qualification :</label>
-                <select className='register-input' defaultValue={""} onChange={handleInput}>
+                <select className='register-input' defaultValue={""} onChange={handleInput} name='qualification'>
                     <option disabled value="">Please select your qualification</option>
                     <option>Under Graduate</option>
                     <option>Post Graduate</option>
